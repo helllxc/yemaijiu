af3ccb6bb084344c3aade506fd5fd06f76b6be81
helllxc <vampirelxc@163.com>
2017-01-13 20:46:47 +0800

    创建login

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// 请求模块
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// 创建任务（执行任务）
+// 目的：编译sass文件
+gulp.task('buildSass',function(){
+	// 查找需要编译的文件
+	gulp.src('src/sass/*.scss')
+
+		// 编译scss文件
+		.pipe(sass({outputStyle:'expanded'}))
+
+		// 输出文件
+		.pipe(gulp.dest('src/css'))
+
+});
+
+
+// 监听sass文件
+gulp.task('jtSass',function(){
+	// 监听文件，当文件有修改时，执行buildSass任务
+	gulp.watch('src/sass/*.scss',['buildSass']);
+})
