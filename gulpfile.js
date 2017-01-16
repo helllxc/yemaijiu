1de2e517951ff9b6150d04bd70a245e6ed4d9394
helllxc <vampirelxc@163.com>
2017-01-16 12:02:25 +0800

    修改gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // 请求模块
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
 // 创建任务（执行任务）
 // 目的：编译sass文件
@@ -13,6 +14,9 @@ gulp.task('buildSass',function(){
 
 		// 输出文件
 		.pipe(gulp.dest('src/css'))
+		
+		//编译成功后刷新利用browser-sync刷新页面
+		.pipe(browserSync.reload({stream:true}))
 
 });
 
@@ -20,5 +24,19 @@ gulp.task('buildSass',function(){
 // 监听sass文件
 gulp.task('jtSass',function(){
 	// 监听文件，当文件有修改时，执行buildSass任务
+	gulp.watch('src/sass/*.scss',['buildSass']);
+})
+
+//创建任务（执行任务）
+gulp.task('server',function(){
+	browserSync({
+		server:{
+			baseDir:"./src"
+		},
+		post:4000,
+		//proxy:'http://localhost/',
+		//添加html文件
+		files:['./src/*.html'],
+	});
 	gulp.watch('src/sass/*.scss',['buildSass']);
 })
